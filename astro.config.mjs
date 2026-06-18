d36eb7fc970719b725879a211caaf6b8a416d5cd
Stewart <stewart.wilson79@gmail.com>
2026-06-18 19:18:41 +0100

    fix: correct GitHub username in site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://stewartwilson.github.io',
+  site: 'https://stewdesign.github.io',
   base: '/design-system-docs',
   integrations: [mdx()],
 });
